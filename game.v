c8bf0fa7a27e71f586ea694cfc2950596692ce8d
zakuro <z@kuro.red>
2021-02-19 20:36:48 +0900

    Add game.v

---
+++ game.v
@@ -0,0 +1,49 @@
+module vna
+
+import gg
+import gx
+
+pub struct Game {
+pub mut:
+	context Context
+mut:
+	gg &gg.Context
+}
+
+pub struct GameConfig {
+	width  int    = 600
+	height int    = 480
+	title  string = 'Game'
+}
+
+pub struct Context {}
+
+pub fn new(c GameConfig) &Game {
+	mut game := &Game{
+		gg: 0
+		context: Context{}
+	}
+	game.gg = gg.new_context(
+		bg_color: gx.Color{
+			r: 100
+			g: 149
+			b: 237
+		}
+		width: c.width
+		height: c.height
+		create_window: true
+		window_title: c.title
+		frame_fn: frame
+		init_fn: init
+		user_data: game
+	)
+	return game
+}
+
+fn frame(_ &Game) {}
+
+fn init(_ &Game) {}
+
+pub fn (g &Game) run() {
+	g.gg.run()
+}
