099440bf498f8d562aa626bc06fb30045608677e
zakuro <z@kuro.red>
2021-02-23 15:12:50 +0900

    split files

--- game.v
+++ game.v
@@ -10,51 +10,6 @@ pub interface Game {
 	draw(ctx &Context)
 }
 
-pub interface GameComponent {
-	init(ctx &Context)
-	update(ctx &Context)
-	draw(ctx &Context)
-}
-
-pub struct ComponentManager {
-mut:
-	store map[string]GameComponent
-
-	not_inited_components []&GameComponent
-}
-
-pub fn (mut m ComponentManager) add(name string, c GameComponent) {
-	m.store[name] = c
-	m.not_inited_components << &c
-}
-
-pub fn (m &ComponentManager) get(name string) ?GameComponent {
-	return m.store[name]
-}
-
-pub fn (mut m ComponentManager) init(ctx &Context) {
-	for _, c in m.store {
-		c.init(ctx)
-	}
-	m.not_inited_components = []
-}
-
-pub fn (mut m ComponentManager) update(ctx &Context) {
-	for c in m.not_inited_components {
-		c.init(ctx)
-	}
-	m.not_inited_components = []
-	for _, c in m.store {
-		c.update(ctx)
-	}
-}
-
-pub fn (m &ComponentManager) draw(ctx &Context) {
-	for _, c in m.store {
-		c.draw(ctx)
-	}
-}
-
 pub struct Context {
 mut:
 	game       Game
