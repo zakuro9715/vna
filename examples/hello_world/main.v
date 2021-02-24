a9bf418e9ca6387804bb1ee4c940445563be3077
zakuro <z@kuro.red>
2021-02-24 19:47:12 +0900

    draw rect example

--- examples/hello_world/main.v
+++ examples/hello_world/main.v
@@ -1,17 +1,18 @@
 module main
 
-import zakuro9715.vna
+import zakuro9715.vna { Context, Rect }
 
 struct Game {
 }
 
-fn (g &Game) init(ctx &vna.Context) {
+fn (g &Game) init(ctx &Context) {
 }
 
-fn (g &Game) update(ctx &vna.Context) {
+fn (g &Game) update(ctx &Context) {
 }
 
-fn (g &Game) draw(ctx &vna.Context) {
+fn (g &Game) draw(ctx &Context) {
+	Rect{10, 10, 200, 100}.draw(ctx, r: 255, g: 0, b: 0)
 }
 
 fn main() {
