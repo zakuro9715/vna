102c7a278d35c947349b604375cc59834b9b19e4
zakuro <z@kuro.red>
2021-02-24 14:43:03 +0900

    add rect

--- data.v
+++ data.v
@@ -23,3 +23,63 @@ pub fn (lhs Point) - (rhs Point) Point {
 		y: lhs.y - rhs.y
 	}
 }
+
+pub struct Size {
+pub:
+	width  int
+	height int
+}
+
+pub fn (s Size) is_zero() bool {
+	return s.width == 0 && s.height == 0
+}
+
+pub fn (lhs Size) + (rhs Size) Size {
+	return {
+		width: lhs.width + rhs.width
+		height: lhs.height + rhs.height
+	}
+}
+
+pub fn (lhs Size) - (rhs Size) Size {
+	return {
+		width: lhs.width - rhs.width
+		height: lhs.height - rhs.height
+	}
+}
+
+pub struct Rect {
+pub:
+	x      int
+	y      int
+	width  int
+	height int
+}
+
+pub fn (r Rect) pos() Point {
+	return Point{r.x, r.y}
+}
+
+pub fn (r Rect) size() Size {
+	return Size{r.width, r.height}
+}
+
+pub fn (r Rect) area() int {
+	return r.width * r.height
+}
+
+pub fn (r Rect) left() int {
+	return r.x
+}
+
+pub fn (r Rect) right() int {
+	return r.x + r.width
+}
+
+pub fn (r Rect) top() int {
+	return r.y
+}
+
+pub fn (r Rect) bottom() int {
+	return r.y + r.height
+}
--- data_test.v
+++ data_test.v
@@ -8,3 +8,24 @@ fn test_point() {
 	assert !Point{1, 0}.is_zero()
 	assert !Point{0, 1}.is_zero()
 }
+
+fn test_size() {
+	assert Size{1, 2} + Size{10, 11} == Size{11, 13}
+	assert Size{1, 2} - Size{10, 11} == Size{-9, -9}
+	assert Size{10, 11} - Size{1, 2} == Size{9, 9}
+	assert Size{}.is_zero()
+	assert !Size{1, 0}.is_zero()
+	assert !Size{0, 1}.is_zero()
+}
+
+fn test_rect() {
+	rect := Rect{2, 3, 4, 5}
+	assert rect.pos() == Point{2, 3}
+	assert rect.size() == Size{4, 5}
+	assert rect.area() == 20
+
+	assert rect.top() == 3
+	assert rect.bottom() == 8
+	assert rect.left() == 2
+	assert rect.right() == 6
+}
