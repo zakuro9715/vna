caf6cf8cc8e1957ef962d2b88b6d3d757da3ee37
zakuro <z@kuro.red>
2021-02-24 19:23:54 +0900

    split files

---
+++ shape.v
@@ -0,0 +1,46 @@
+module vna
+
+pub struct Rect {
+pub:
+	x      int
+	y      int
+	width  int
+	height int
+}
+
+pub fn (r Rect) extend(size Size) Rect {
+	return Rect{
+		x: r.x
+		y: r.y
+		width: r.width + size.width
+		height: r.height + size.height
+	}
+}
+
+pub fn (r Rect) pos() Point {
+	return Point{r.x, r.y}
+}
+
+pub fn (r Rect) size() Size {
+	return Size{r.width, r.height}
+}
+
+pub fn (r Rect) area() int {
+	return r.width * r.height
+}
+
+pub fn (r Rect) left() int {
+	return r.x
+}
+
+pub fn (r Rect) right() int {
+	return r.x + r.width
+}
+
+pub fn (r Rect) top() int {
+	return r.y
+}
+
+pub fn (r Rect) bottom() int {
+	return r.y + r.height
+}
--- structs_test.v
+++ structs_test.v
@@ -17,17 +17,3 @@ fn test_size() {
 	assert !Size{1, 0}.is_zero()
 	assert !Size{0, 1}.is_zero()
 }
-
-fn test_rect() {
-	rect := Rect{2, 3, 4, 5}
-	assert rect.pos() == Point{2, 3}
-	assert rect.size() == Size{4, 5}
-	assert rect.area() == 20
-
-	assert rect.top() == 3
-	assert rect.bottom() == 8
-	assert rect.left() == 2
-	assert rect.right() == 6
-
-	assert rect.extend(width: 3, height: 4) == Rect{2, 3, 7, 9}
-}
