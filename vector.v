a2109807abfeaeaf66ed148be3e7dc28c180195d
zakuro <z@kuro.red>
2021-02-25 14:21:12 +0900

    vec2: add * and /

--- vector.v
+++ vector.v
@@ -20,6 +20,14 @@ pub fn (lhs Vec2) - (rhs Vec2) Vec2 {
 	return Vec2{lhs.x - rhs.x, lhs.y - rhs.y}
 }
 
+pub fn (lhs Vec2) * (rhs Vec2) Vec2 {
+	return Vec2{lhs.x * rhs.x, lhs.y * rhs.y}
+}
+
+pub fn (lhs Vec2) / (rhs Vec2) Vec2 {
+	return Vec2{lhs.x / rhs.x, lhs.y / rhs.y}
+}
+
 pub fn (lhs Vec2) mul(rhs f32) Vec2 {
 	return Vec2{lhs.x * rhs, lhs.y * rhs}
 }
--- vector_test.v
+++ vector_test.v
@@ -6,8 +6,11 @@ fn test_vec2() {
 	assert !Vec2{0, 1}.is_zero()
 
 	assert (Vec2{1, 1} + Vec2{-1, -1}).is_zero()
+
 	assert Vec2{1, 2} + Vec2{3, 4} == Vec2{4, 6}
 	assert Vec2{1, 2} - Vec2{3, 4} == Vec2{-2, -2}
+	assert Vec2{3, 4} * Vec2{3, 2} == Vec2{9, 8}
+	assert Vec2{3, 4} / Vec2{3, 2} == Vec2{1, 2}
 	assert Vec2{1, 2}.mul(1.5) == Vec2{1.5, 3}
 	assert Vec2{1.5, 3}.div(1.5) == Vec2{1, 2}
 	assert Vec2{1, 2}.negative() == Vec2{-1, -2}
