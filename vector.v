004f68b884c568728974d3804fc322282f7137cd
zakuro <z@kuro.red>
2021-02-25 13:34:08 +0900

    vector2: add operators

--- vector.v
+++ vector.v
@@ -9,3 +9,23 @@ pub:
 pub fn (v Vector2) is_zero() bool {
 	return v.x == 0 && v.y == 0
 }
+
+pub fn (lhs Vector2) + (rhs Vector2) Vector2 {
+	return Vector2{lhs.x + rhs.x, lhs.y + rhs.y}
+}
+
+pub fn (lhs Vector2) - (rhs Vector2) Vector2 {
+	return Vector2{lhs.x - rhs.x, lhs.y - rhs.y}
+}
+
+pub fn (lhs Vector2) mul(rhs f32) Vector2 {
+	return Vector2{lhs.x * rhs, lhs.y * rhs}
+}
+
+pub fn (lhs Vector2) div(rhs f32) Vector2 {
+	return Vector2{lhs.x / rhs, lhs.y / rhs}
+}
+
+pub fn (v Vector2) negative() Vector2 {
+	return v.mul(-1)
+}
--- vector_test.v
+++ vector_test.v
@@ -5,3 +5,12 @@ fn test_vector2() {
 	assert !Vector2{1, 0}.is_zero()
 	assert !Vector2{0, 1}.is_zero()
 }
+
+fn test_vector2_operators() {
+	assert (Vector2{1, 1} + Vector2{-1, -1}).is_zero()
+	assert Vector2{1, 2} + Vector2{3, 4} == Vector2{4, 6}
+	assert Vector2{1, 2} - Vector2{3, 4} == Vector2{-2, -2}
+	assert Vector2{1, 2}.mul(1.5) == Vector2{1.5, 3}
+	assert Vector2{1.5, 3}.div(1.5) == Vector2{1, 2}
+	assert Vector2{1, 2}.negative() == Vector2{-1, -2}
+}
