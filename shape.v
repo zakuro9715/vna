ae41fac0fbfe2bc2acba0db1dab3afdda181f199
zakuro <z@kuro.red>
2021-02-25 17:02:11 +0900

    remove point and size

--- shape.v
+++ shape.v
@@ -2,46 +2,46 @@ module vna
 
 pub struct Rect {
 pub:
-	x      int
-	y      int
-	width  int
-	height int
+	x      f32
+	y      f32
+	width  f32
+	height f32
 }
 
-pub fn (r Rect) extend(size Size) Rect {
+pub fn (r Rect) extend(p Vec2) Rect {
 	return Rect{
 		x: r.x
 		y: r.y
-		width: r.width + size.width
-		height: r.height + size.height
+		width: r.width + p.x
+		height: r.height + p.y
 	}
 }
 
-pub fn (r Rect) pos() Point {
-	return Point{r.x, r.y}
+pub fn (r Rect) pos() Vec2 {
+	return Vec2{r.x, r.y}
 }
 
-pub fn (r Rect) size() Size {
-	return Size{r.width, r.height}
+pub fn (r Rect) size() Vec2 {
+	return Vec2{r.width, r.height}
 }
 
-pub fn (r Rect) area() int {
+pub fn (r Rect) area() f32 {
 	return r.width * r.height
 }
 
-pub fn (r Rect) left() int {
+pub fn (r Rect) left() f32 {
 	return r.x
 }
 
-pub fn (r Rect) right() int {
+pub fn (r Rect) right() f32 {
 	return r.x + r.width
 }
 
-pub fn (r Rect) top() int {
+pub fn (r Rect) top() f32 {
 	return r.y
 }
 
-pub fn (r Rect) bottom() int {
+pub fn (r Rect) bottom() f32 {
 	return r.y + r.height
 }
 
--- shape_test.v
+++ shape_test.v
@@ -2,8 +2,8 @@ module vna
 
 fn test_rect() {
 	rect := Rect{2, 3, 4, 5}
-	assert rect.pos() == Point{2, 3}
-	assert rect.size() == Size{4, 5}
+	assert rect.pos() == Vec2{2, 3}
+	assert rect.size() == Vec2{4, 5}
 	assert rect.area() == 20
 
 	assert rect.top() == 3
@@ -11,5 +11,5 @@ fn test_rect() {
 	assert rect.left() == 2
 	assert rect.right() == 6
 
-	assert rect.extend(width: 3, height: 4) == Rect{2, 3, 7, 9}
+	assert rect.extend(x: 3, y: 4) == Rect{2, 3, 7, 9}
 }
