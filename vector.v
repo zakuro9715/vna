28cc5a54e2fc1d0e8b59f7b34ea12c1f9f853a1f
zakuro <z@kuro.red>
2021-02-25 13:35:36 +0900

    Vector2 -> Vec2

--- vector.v
+++ vector.v
@@ -1,31 +1,31 @@
 module vna
 
-pub struct Vector2 {
+pub struct Vec2 {
 pub:
 	x f32
 	y f32
 }
 
-pub fn (v Vector2) is_zero() bool {
+pub fn (v Vec2) is_zero() bool {
 	return v.x == 0 && v.y == 0
 }
 
-pub fn (lhs Vector2) + (rhs Vector2) Vector2 {
-	return Vector2{lhs.x + rhs.x, lhs.y + rhs.y}
+pub fn (lhs Vec2) + (rhs Vec2) Vec2 {
+	return Vec2{lhs.x + rhs.x, lhs.y + rhs.y}
 }
 
-pub fn (lhs Vector2) - (rhs Vector2) Vector2 {
-	return Vector2{lhs.x - rhs.x, lhs.y - rhs.y}
+pub fn (lhs Vec2) - (rhs Vec2) Vec2 {
+	return Vec2{lhs.x - rhs.x, lhs.y - rhs.y}
 }
 
-pub fn (lhs Vector2) mul(rhs f32) Vector2 {
-	return Vector2{lhs.x * rhs, lhs.y * rhs}
+pub fn (lhs Vec2) mul(rhs f32) Vec2 {
+	return Vec2{lhs.x * rhs, lhs.y * rhs}
 }
 
-pub fn (lhs Vector2) div(rhs f32) Vector2 {
-	return Vector2{lhs.x / rhs, lhs.y / rhs}
+pub fn (lhs Vec2) div(rhs f32) Vec2 {
+	return Vec2{lhs.x / rhs, lhs.y / rhs}
 }
 
-pub fn (v Vector2) negative() Vector2 {
+pub fn (v Vec2) negative() Vec2 {
 	return v.mul(-1)
 }
--- vector_test.v
+++ vector_test.v
@@ -1,16 +1,16 @@
 module vna
 
-fn test_vector2() {
-	assert Vector2{0, 0}.is_zero()
-	assert !Vector2{1, 0}.is_zero()
-	assert !Vector2{0, 1}.is_zero()
+fn test_vec2() {
+	assert Vec2{0, 0}.is_zero()
+	assert !Vec2{1, 0}.is_zero()
+	assert !Vec2{0, 1}.is_zero()
 }
 
-fn test_vector2_operators() {
-	assert (Vector2{1, 1} + Vector2{-1, -1}).is_zero()
-	assert Vector2{1, 2} + Vector2{3, 4} == Vector2{4, 6}
-	assert Vector2{1, 2} - Vector2{3, 4} == Vector2{-2, -2}
-	assert Vector2{1, 2}.mul(1.5) == Vector2{1.5, 3}
-	assert Vector2{1.5, 3}.div(1.5) == Vector2{1, 2}
-	assert Vector2{1, 2}.negative() == Vector2{-1, -2}
+fn test_vec2_operators() {
+	assert (Vec2{1, 1} + Vec2{-1, -1}).is_zero()
+	assert Vec2{1, 2} + Vec2{3, 4} == Vec2{4, 6}
+	assert Vec2{1, 2} - Vec2{3, 4} == Vec2{-2, -2}
+	assert Vec2{1, 2}.mul(1.5) == Vec2{1.5, 3}
+	assert Vec2{1.5, 3}.div(1.5) == Vec2{1, 2}
+	assert Vec2{1, 2}.negative() == Vec2{-1, -2}
 }
